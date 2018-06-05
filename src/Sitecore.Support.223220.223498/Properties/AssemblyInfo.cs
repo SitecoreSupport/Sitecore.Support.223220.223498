a35c32afb6080c847a513037b204f219baaa2dfd
DK\mao <mao@sitecore.net>
2018-06-05 10:39:02 +0300

    Initial commit

---
+++ src/Sitecore.Support.223220.223498/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.223220.223498")]
+[assembly: AssemblyProduct("Sitecore.Support.223220.223498")]
+[assembly: ComVisible(false)]
